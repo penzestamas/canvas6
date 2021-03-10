dddbd5d1ef5702dcfcde648a1464dbaef8bab6b3
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-10 11:22:13 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var positionX = 150;
+var positionY = 225;
+var size = 150;
+
+context.beginPath();
+context.moveTo(positionX, positionY);
+context.lineTo(positionX + size, positionY);
+context.lineTo(positionX + size / 2, positionY - size);
+context.lineTo(positionX, positionY);
+context.strokeStyle = 'rgba(128,128,128,.5)';
+context.stroke();
+context.fillStyle = 'rgba(255,165,0,.5)';
+context.fill();
